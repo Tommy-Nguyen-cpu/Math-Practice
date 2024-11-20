d84ebbdeb9bb041566d325fe62570058fef0367a
Tommy-Nguyen <75864631+Tommy-Nguyen-cpu@users.noreply.github.com>
2024-11-20 18:24:52 -0500

    Example Augmented Matrix Operation
    * Added an example script for showing how to solve linear systems of equations via augmented matrices.

--- Linear Algebra/Systems of Equations (Chapter 1)/SystemsOfEquations.py
+++ Linear Algebra/Systems of Equations (Chapter 1)/SystemsOfEquations.py
@@ -28,4 +28,76 @@ def definingSystemsOfEquations():
                 # 1. They meet at exactly 1 point.
                 # 2. They are parallel and as such, do not intersect at all.
                 # 3. They are the same line, as such they have infinite intersections.
-        
+        # 5. Consistent - a system of linear equation is said to be consistent if it has atleast 1 solution.
+        # 6. Inconsistent - a system of linear equation is said to be inconsistent if it has NO solution.
+        # 7. These definitions can be extended to ALL linear systems of equations: they all can have 0, 1, or infinitely many solutions. There are no other options.
+
+    # EX: Given the following equations 10x + 20y = 4 and 30x + 40y = 7, find x and y coordinates.
+    equations = np.array(
+        [[10, 20],
+            [30, 40]]
+    )
+
+    constants = np.array([4, 7])
+
+    row1 = equations[0]
+    row2 = equations[1]
+
+    factor = -row2[0] / row1[0] # Grab factor, which is -3
+
+    row_new = row1 * factor + row2 # (10x + 20y) * -3 + (30x + 40y) = -20y
+    b_new = constants[0] * factor + constants[1] # 4 * -3 + 7 = -5
+    y = b_new / row_new[1] # -5 / -20 = y
+    x = (constants[0] - row1[1] * y) / row1[0] # (4 - 20 * (5/20)) / 10 = x
+
+    # Notice how we are able to come to a single solution. There are some cases where there are infinitely many solutions.
+    # In this case, we can solve for any of the variables, and assign an arbitrary value "t", "r", etc (also called a PARAMETER) to other unsolved variables.
+    # Basically, these other variables can have any value, as long as it solves the system of equations.
+    # For example:
+    # x + 2y = 2
+    # 2x + 4y = 4 <- Notice how the second equation is literally just the first equation multiplied by 2. This means that the second equation holds no value and can be omitted.
+    # Would be an infinitely many solutions, and can be rewritten as a single equation:
+    # x = 2 - 2t, y = t <- if we solve for x.
+    # y = 1 - (1/2)t, x = t <- if we solve for y
+    # If we went with equation 1 (solving for x) and plugged in t = 1, our solution would be:
+    # (0, 1) = (x, y)
+
+    # Now things can be a bit cooler:
+    # Now that we know the fundamentals, we can start representing these equations as matrices!
+    # Why?
+    # As more variables and equations are introduced, it becomes computationally expensive to keep track of every individual operations, numbers, etc.
+    # Instead, we can maintain a matrix that excludes operations and variable names.
+    '''
+    [
+    [20 30 40 6]
+    [10 30 20 1]
+    ...
+    ]
+    '''
+    # The example matrix above may represent a large linear system of equations.
+    # 20, 30, 40, and 6 would represent: 20x + 30y + 40z = 6.
+    # This type of matrix is called an "AUGMENTED MATRIX".
+    # For augmented matrices, we solve the system of linear equations through operations on the rows.
+
+def solve_linear_equations(matrix):
+    for i in range(len(matrix)):
+        for j in range(i + 1, len(matrix)):
+            perform_row_operations(matrix, i, j)
+    for i in range(len(matrix) - 1, -1, -1):
+        for j in range(len(matrix) - 2, -1, -1):
+            perform_row_operations(matrix, i, j)
+    
+    print(matrix)
+
+def perform_row_operations(matrix, i, j):
+    factor = -1 * matrix[j][i] / matrix[i][i]
+    print(f"step 1, calculate factor: -1 * {matrix[j][i]} / {matrix[i][i]}")
+    matrix[j] = matrix[j] + matrix[i] * factor
+    print(f"step 2, calculate new row: {matrix[j]} + {matrix[i]} * {factor}")
+    print(matrix)
+    print()
+solve_linear_equations(
+    np.array([[1, 1, 2, 9], 
+     [2, 4, -3, 1], 
+      [3, 6, -5, 0]], dtype=np.float16)
+)
