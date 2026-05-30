de215811775711fee6603069246e3de155c452a7
Tommy-Nguyen <75864631+Tommy-Nguyen-cpu@users.noreply.github.com>
2026-05-30 13:49:41 -0400

    Final For Properties Impl (Probably..)
    * Implemented some of the remaining properties. Could implement matrix polynomial...but will leave that for the future me to decide.
    * Minor fix to symbol in doc.

--- Linear Algebra/Algebraic Properties/properties.py
+++ Linear Algebra/Algebraic Properties/properties.py
@@ -51,7 +51,38 @@ def verify_product_of_transpose(matrixA, matrixB):
     print(f"Transposed Product:\n {transposed_product}")
     print(f"Product of transposes:\n {product_of_transposes}")
 
+def matrix_multiply(matrixA, matrixB):
+    result = np.zeros((len(matrixA), len(matrixB[0])))
+    for i in range(len(matrixA)):
+        sum_val = 0
+        for j in range(len(matrixB[0])):
+            sum_val += matrixA[i][j] * matrixB[j][i]
+        result[i][i] = sum_val
+    return result
+
+def verify_product_of_inverse(matrixA, matrixB):
+    inverseA = find_inverse(matrixA)
+    inverseB = find_inverse(matrixB)
+
+    product_of_inverses = matrix_multiply(inverseA, inverseB)
+    inverse_of_product = matrix_multiply(product_of_inverses, matrix_multiply(matrixA, matrixB))
+    inverse_of_product_non_inverse_first = matrix_multiply(matrix_multiply(matrixA, matrixB), product_of_inverses)
+    print(f"Inverse of product:\n {inverse_of_product}")
+    print(f"Inverse of product (non-inverse first):\n {inverse_of_product_non_inverse_first}")
+
+def verify_inverse_of_transpose_property(matrixA):
+    transpose = np.transpose(matrixA)
+    inverse_of_transpose = find_inverse(transpose)
+
+    inverse  = find_inverse(matrixA)
+    transpose_of_inverse = np.transpose(inverse)
+
+    print(f"Inverse of transpose:\n {inverse_of_transpose}")
+    print(f"Transpose of inverse:\n {transpose_of_inverse}")
+
 inverse = find_inverse(np.array([[4, 7], [2, 6]]))
 verify_inverse_numpy(np.array([[4, 7], [2, 6]]), inverse)
 verify_inverse(np.array([[4, 7], [2, 6]]), inverse)
 verify_product_of_transpose(np.array([[4, 7], [2, 6]]), np.array([[1, 2], [3, 4]]))
+verify_product_of_inverse(np.array([[4, 7], [2, 6]]), np.array([[1, 2], [3, 4]]))
+verify_inverse_of_transpose_property(np.array([[4, 7], [2, 6]]))
